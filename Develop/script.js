0aed9125ef800c538d14fd69d681722e0f9d39f9
jackhanson510@gmail.com <johnhanson@Johns-MacBook-Pro-4.local>
2022-12-12 19:48:28 -0500

    Added Javascript code for generator character questions

--- Develop/script.js
+++ Develop/script.js
@@ -1,3 +1,51 @@
+function generatePassword() {
+
+var minlength = 8;
+var maxlength = 128;
+var pword = "";
+
+var length = prompt("Enter Pasword length(minimum " + minlength + ",maximum" + maxlength +"):");
+
+if (length < minlength || length > maxlength) {
+  alert("Invalid Password length.  Please try again")
+}
+
+var characterset = "";
+
+var includeLowercase = confirm("Include lowercase characters?");
+if (includeLowercase) {
+  characterset += "abcdefghijklmnopqrstuvwxyz";
+}
+
+var includeUppercase = confirm("Include Uppercase characters?");
+if (includeUppercase) {
+  characterset += "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+}
+
+var includenumber = confirm("Include Numeric Character?");
+if (includenumber) {
+  characterset += "0123456789";
+}
+
+var includeSpecial = confirm("Include Special Character?")
+if (includeSpecial) {
+  characterset += "!@#$%^&*()?";
+}
+
+if (characterset === "") {
+  alert("At least one character type must be selected")
+}
+
+for (var i = 0; i < length; i++) {
+  pword += characterset.charAt(Math.floor(Math.random() * characterset.length))
+}
+
+return pword
+}
+
+
+
+
 // Assignment code here
 
 
